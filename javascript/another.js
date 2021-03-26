45a82e08828cc7c7f3539b1faa940536a6c9106b
appaxisllc <68441201+appaxisllc@users.noreply.github.com>
2021-03-26 11:51:53 -0400

    Nodes not uploaded
    You will need to install the nodes into the main working folder to reproduce.

---
+++ javascript/another.js
@@ -0,0 +1,10 @@
+//index.js
+
+function sayHello(){
+    let header = document.querySelector("#greeting");
+    let hello = ("<h1> hello world </h1>");
+    header.innerHTML = hello;
+  }
+  
+  module.exports = sayHello();
+ 
---
+++ javascript/index.js
@@ -0,0 +1,5 @@
+//index.js
+require("style-loader!css-loader!../css/app.css");
+require('./another.js');
+
+  sayHello();
